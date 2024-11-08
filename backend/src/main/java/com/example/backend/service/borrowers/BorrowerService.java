a80b5a661627c504d23585cb2654a7f3849e889b
= <phuongnguyen20904@gmail.com>
2024-11-08 14:07:02 +0700

    refactor: organize code service of Borrower

--- backend/src/main/java/com/example/backend/service/borrowers/BorrowerService.java
+++ backend/src/main/java/com/example/backend/service/borrowers/BorrowerService.java
@@ -1,17 +1,17 @@
-package com.example.backend.service.borrowers;
-
-import com.example.backend.model.Borrower;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-public class BorrowerService implements IBorrowerService {
-    @Autowired
-    private
-    @Override
-    public List<Borrower> getBorrowers() {
-        return List.of();
-    }
-}
+//package com.example.backend.service.borrowers;
+//
+//import com.example.backend.model.Borrower;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//public class BorrowerService implements IBorrowerService {
+//    @Autowired
+//    private
+//    @Override
+//    public List<Borrower> getBorrowers() {
+//        return List.of();
+//    }
+//}
