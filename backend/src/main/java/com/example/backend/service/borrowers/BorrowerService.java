f53875df4439d6c0c7991fd5a60ab2065fa25abb
= <phuongnguyen20904@gmail.com>
2024-11-08 15:01:54 +0700

    feat: add methods for BorrowerService and IBorrowerService Add: getAllBorrowers getBorrowerById createBorrower updateBorrower deleteBorrower

--- backend/src/main/java/com/example/backend/service/borrowers/BorrowerService.java
+++ backend/src/main/java/com/example/backend/service/borrowers/BorrowerService.java
@@ -1,17 +1,45 @@
-//package com.example.backend.service.borrowers;
-//
-//import com.example.backend.model.Borrower;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//
-//@Service
-//public class BorrowerService implements IBorrowerService {
-//    @Autowired
-//    private
-//    @Override
-//    public List<Borrower> getBorrowers() {
-//        return List.of();
-//    }
-//}
+package com.example.backend.service.borrowers;
+
+import com.example.backend.dto.BorrowerDTO;
+import com.example.backend.mapper.LibraryMapper;
+import com.example.backend.model.Borrower;
+import com.example.backend.service.books.BookService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BorrowerService implements IBorrowerService {
+    private static final Logger logger = LoggerFactory.getLogger(BorrowerService.class);
+
+    @Autowired
+    private LibraryMapper libraryMapper;
+
+    @Override
+    public List<Borrower> getAllBorrowers() {
+        return List.of();
+    }
+
+    @Override
+    public BorrowerDTO getBorrowerById(Long borrowerID) {
+        return null;
+    }
+
+    @Override
+    public BorrowerDTO createBorrower(BorrowerDTO borrowerDTO) {
+        return null;
+    }
+
+    @Override
+    public BorrowerDTO updateBorrower(Long borrowerID, BorrowerDTO borrowerDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteBorrower(Long borrowerDTO) {
+
+    }
+}
