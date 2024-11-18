896ee2260f6607fe0f43e1ff6927dc4cba55f3e6
Hueyyyy <khatihuy125@gmail.com>
2024-11-19 02:27:53 +0700

    add redux-persist

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,17 +4,18 @@ import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './store/Store.ts'
-// import { persistor, store } from './store/store.ts'
-// import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor, store } from './store/Store.ts'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </PersistGate>
     </Provider>
   </StrictMode>,
 )
--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,18 +1,32 @@
 import CounterReducer from "./reducers/CounterReducers";
 import { thunk } from "redux-thunk";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 
+const persistConfig = {
+  key: 'root', // Key for storage
+  storage,
+}
 
 const rootReducer = combineReducers({
   CounterStore: CounterReducer,
 });
 
-
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore redux-persist actions in serializability check
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }).concat(thunk),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
