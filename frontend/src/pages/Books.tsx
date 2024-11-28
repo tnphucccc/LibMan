8cfba7a651ba7dddf232b3a3f322302cca0de003
Khang Huy <khatihuy125@gmail.com>
2024-11-28 15:49:08 +0700

    pull main

--- frontend/src/pages/Books.tsx
+++ frontend/src/pages/Books.tsx
@@ -7,7 +7,9 @@ export default function Books() {
 
     const [bookList, setBookList] = useState([]);
     const [isShowModal, setIsShowModal] = useState(false);
+    const [currentBookId, setCurrentBookId] = useState<number>();
     const idRef = useRef<HTMLInputElement>(null);
+    const dateRef = useRef<HTMLInputElement>(null);
 
     const handleGetBooks = async () => {  
         try {
@@ -21,7 +23,8 @@ export default function Books() {
         }
     };
 
-    const handleOpenModal = () => {
+    const handleOpenModal = (id:number) => {
+        setCurrentBookId(id);
         setIsShowModal(true);
     };
 
@@ -29,8 +32,21 @@ export default function Books() {
         setIsShowModal(false);
     };
 
-    const handleSubmit = () => {
-        console.log(idRef.current?.value);
+    const handleSubmit = async () => {
+        try {
+            const res = await axios.post(import.meta.env.VITE_BASE_URL + '/borrowings', {
+                bookId: currentBookId,
+                borrowerId: parseInt(idRef.current!.value),
+                dueDate: dateRef.current!.value
+            });
+            if (res.status === 200) {
+                console.log(res.data);
+                handleGetBooks();
+            }
+        } catch (error) {
+            console.error(error);
+        }
+        handleCloseModal();
     }
 
     useEffect(() => {
@@ -43,13 +59,13 @@ export default function Books() {
             <div className="flex flex-row flex-wrap w-full h-fit gap-4 mt-4 justify-center">
                 { bookList.map((book: any) => (
                 <div key={book.id} className="border-2 border-black w-72 h-96 p-2 rounded-lg">
-                    <img src="https://marketplace.canva.com/EAFfSnGl7II/2/0/1003w/canva-elegant-dark-woods-fantasy-photo-book-cover-vAt8PH1CmqQ.jpg" className="w-full h-56"/>
+                    <img src={book.coverImageUrl} className="w-full h-56"/>
                     <h3 className="font-semibold text-base truncate">{book.title}</h3>
                     <p className="text-sm">{book.publicationYear < 0? `${book.publicationYear.toString().substring(1,book.publicationYear.length)} BC`: book.publicationYear}</p>
                     <p className="text-sm">{book.authors[0].name}</p>
                     <p className="text-sm">{book.isbn}</p>
                     <div className="flex justify-end p-2">
-                        <button className={`border-2 border-black p-2 text-base font-semibold rounded-lg bg-black text-white ${book.status == "AVAILABLE" && "hover:bg-white hover:text-black"}  ${book.status != "AVAILABLE" && "cursor-not-allowed opacity-50"}`} disabled = {book.status != "AVAILABLE"? true:false} onClick={() => handleOpenModal()}>Borrow</button>
+                        <button className={`border-2 border-black p-2 text-base font-semibold rounded-lg bg-black text-white ${book.status == "AVAILABLE" && "hover:bg-white hover:text-black"}  ${book.status != "AVAILABLE" && "cursor-not-allowed opacity-50"}`} disabled = {book.status != "AVAILABLE"? true:false} onClick={() => handleOpenModal(book.bookId)}>Borrow</button>
                     </div>
                 </div>
             ))}
@@ -75,6 +91,11 @@ export default function Books() {
                             <label className="ml-3 opacity-50">Borrower ID</label>
                             <input className="mt-1 h-10 w-full rounded-lg pl-3 border-2 border-black font-semibold"
                             type="text" ref={idRef}/>
+                        </div>
+                        <div className="flex flex-col text-sm">
+                            <label className="ml-3 opacity-50">Due date</label>
+                            <input className="mt-1 h-10 w-full rounded-lg pl-3 border-2 border-black font-semibold"
+                            type="date" ref={dateRef}/>
                         </div>
                     </div>
                     <div className="flex justify-end gap-2 mt-4">
