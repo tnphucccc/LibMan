7ddc9b5c3d87dcff2778cac9ed48503c8b647bf5
tnphucccc <ktsukasa1412@gmail.com>
2024-11-28 15:08:18 +0700

    chores: update books cover

--- backend/src/main/resources/query.sql
+++ backend/src/main/resources/query.sql
@@ -28,44 +28,44 @@ VALUES ('George Orwell', 'British', 'https://example.com/portraits/george_orwell
        ('Alexandre Dumas', 'French', 'https://example.com/authors/dumas.jpg');
 
 INSERT INTO books (title, isbn, publication_year, status, cover_image_url)
-VALUES ('1984', '9780451524935', 1949, 'AVAILABLE', 'https://example.com/covers/1984.jpg'),
-       ('Pride and Prejudice', '9780141199078', 1813, 'AVAILABLE', 'https://example.com/covers/pride_prejudice.jpg'),
+VALUES ('1984', '9780451524935', 1949, 'AVAILABLE', 'https://i.ebayimg.com/images/g/u6wAAOSwwX1hN7xf/s-l1200.jpg'),
+       ('Pride and Prejudice', '9780141199078', 1813, 'AVAILABLE', 'https://s3-ap-southeast-2.amazonaws.com/assets.allenandunwin.com/images/small/9780571337019.jpg'),
        ('Adventures of Huckleberry Finn', '9780486280615', 1884, 'BORROWED',
-        'https://example.com/covers/huckleberry_finn.jpg'),
+        'https://m.media-amazon.com/images/I/91Suc5Kql8L._AC_UF1000,1000_QL80_.jpg'),
        ('Harry Potter and the Sorcerer''s Stone', '9780590353427', 1997, 'AVAILABLE',
-        'https://example.com/covers/harry_potter_1.jpg'),
-       ('One Hundred Years of Solitude', '9780060883287', 1967, 'AVAILABLE', 'https://example.com/covers/solitude.jpg'),
-       ('The Great Gatsby', '9780743273565', 1925, 'AVAILABLE', 'https://example.com/covers/great_gatsby.jpg'),
+        'https://m.media-amazon.com/images/I/81q77Q39nEL._AC_UF1000,1000_QL80_.jpg'),
+       ('One Hundred Years of Solitude', '9780060883287', 1967, 'AVAILABLE', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3r_kq5KFnYkYY-ggqayM1sjmZFQ3gWvuJLw&s'),
+       ('The Great Gatsby', '9780743273565', 1925, 'AVAILABLE', 'https://m.media-amazon.com/images/I/81QuEGw8VPL._AC_UF1000,1000_QL80_.jpg'),
        ('To Kill a Mockingbird', '9780061120084', 1960, 'BORROWED',
-        'https://example.com/covers/to_kill_a_mockingbird.jpg'),
+        'https://m.media-amazon.com/images/I/81aY1lxk+9L._AC_UF1000,1000_QL80_.jpg'),
        ('The Catcher in the Rye', '9780316769488', 1951, 'AVAILABLE',
-        'https://example.com/covers/catcher_in_the_rye.jpg'),
+        'https://m.media-amazon.com/images/I/8125BDk3l9L.jpg'),
        ('The Lord of the Rings', '9780544003415', 1954, 'AVAILABLE',
-        'https://example.com/covers/lord_of_the_rings.jpg'),
-       ('Moby Dick', '9781503280786', 1851, 'AVAILABLE', 'https://example.com/covers/moby_dick.jpg'),
-       ('War and Peace', '9780199232765', 1869, 'AVAILABLE', 'https://example.com/covers/war_and_peace.jpg'),
+        'https://m.media-amazon.com/images/I/7125+5E40JL._AC_UF1000,1000_QL80_.jpg'),
+       ('Moby Dick', '9781503280786', 1851, 'AVAILABLE', 'https://m.media-amazon.com/images/I/51aV053NRjL._AC_UF1000,1000_QL80_.jpg'),
+       ('War and Peace', '9780199232765', 1869, 'AVAILABLE', 'https://m.media-amazon.com/images/I/71lPZpwz0HL._AC_UF1000,1000_QL80_.jpg'),
        ('Crime and Punishment', '9780679734505', 1866, 'BORROWED',
-        'https://example.com/covers/crime_and_punishment.jpg'),
-       ('The Odyssey', '9780140268867', -700, 'AVAILABLE', 'https://example.com/covers/odyssey.jpg'),
-       ('Jane Eyre', '9780141441146', 1847, 'BORROWED', 'https://example.com/covers/jane_eyre.jpg'),
-       ('Brave New World', '9780060850524', 1932, 'AVAILABLE', 'https://example.com/covers/brave_new_world.jpg'),
-       ('Fahrenheit 451', '9781451673319', 1953, 'BORROWED', 'https://example.com/covers/fahrenheit_451.jpg'),
-       ('Wuthering Heights', '9780141439556', 1847, 'AVAILABLE', 'https://example.com/covers/wuthering_heights.jpg'),
-       ('The Picture of Dorian Gray', '9780141439570', 1890, 'AVAILABLE', 'https://example.com/covers/dorian_gray.jpg'),
-       ('The Hobbit', '9780547928227', 1937, 'AVAILABLE', 'https://example.com/covers/hobbit.jpg'),
-       ('Animal Farm', '9780451526342', 1945, 'AVAILABLE', 'https://example.com/covers/animal_farm.jpg'),
-       ('Little Women', '9780142408766', 1868, 'AVAILABLE', 'https://example.com/covers/little_women.jpg'),
-       ('Dracula', '9780141439846', 1897, 'BORROWED', 'https://example.com/covers/dracula.jpg'),
-       ('Frankenstein', '9780486282114', 1818, 'AVAILABLE', 'https://example.com/covers/frankenstein.jpg'),
-       ('The Alchemist', '9780061122415', 1988, 'AVAILABLE', 'https://example.com/covers/alchemist.jpg'),
-       ('The Divine Comedy', '9780140448955', 1320, 'AVAILABLE', 'https://example.com/covers/divine_comedy.jpg'),
-       ('Les Misérables', '9780451419439', 1862, 'AVAILABLE', 'https://example.com/covers/les_miserables.jpg'),
-       ('Don Quixote', '9780060934347', 1605, 'AVAILABLE', 'https://example.com/covers/don_quixote.jpg'),
-       ('A Tale of Two Cities', '9780486406516', 1859, 'BORROWED', 'https://example.com/covers/tale_of_two_cities.jpg'),
+        'https://m.media-amazon.com/images/I/71O2XIytdqL._AC_UF1000,1000_QL80_.jpg'),
+       ('The Odyssey', '9780140268867', -700, 'AVAILABLE', 'https://m.media-amazon.com/images/I/81g0AATkO9L._AC_UF1000,1000_QL80_.jpg'),
+       ('Jane Eyre', '9780141441146', 1847, 'BORROWED', 'https://m.media-amazon.com/images/I/61N-UOA0alL._UF1000,1000_QL80_.jpg'),
+       ('Brave New World', '9780060850524', 1932, 'AVAILABLE', 'https://m.media-amazon.com/images/I/71GNqqXuN3L._AC_UF1000,1000_QL80_.jpg'),
+       ('Fahrenheit 451', '9781451673319', 1953, 'BORROWED', 'https://m.media-amazon.com/images/I/61l8LHt4MeL._AC_UF1000,1000_QL80_.jpg'),
+       ('Wuthering Heights', '9780141439556', 1847, 'AVAILABLE', 'https://m.media-amazon.com/images/I/81unikMK30L._AC_UF1000,1000_QL80_.jpg'),
+       ('The Picture of Dorian Gray', '9780141439570', 1890, 'AVAILABLE', 'https://m.media-amazon.com/images/I/91uns-1GnQL._AC_UF1000,1000_QL80_.jpg'),
+       ('The Hobbit', '9780547928227', 1937, 'AVAILABLE', 'https://m.media-amazon.com/images/I/712cDO7d73L.jpg'),
+       ('Animal Farm', '9780451526342', 1945, 'AVAILABLE', 'https://m.media-amazon.com/images/I/91Lbhwt5RzL._AC_UF1000,1000_QL80_.jpg'),
+       ('Little Women', '9780142408766', 1868, 'AVAILABLE', 'https://m.media-amazon.com/images/I/91j2nOoYm0L.jpg'),
+       ('Dracula', '9780141439846', 1897, 'BORROWED', 'https://m.media-amazon.com/images/I/91wOUFZCE+L._UF1000,1000_QL80_.jpg'),
+       ('Frankenstein', '9780486282114', 1818, 'AVAILABLE', 'https://m.media-amazon.com/images/I/81AuVq270jL._UF1000,1000_QL80_.jpg'),
+       ('The Alchemist', '9780061122415', 1988, 'AVAILABLE', 'https://m.media-amazon.com/images/I/61HAE8zahLL._AC_UF1000,1000_QL80_.jpg'),
+       ('The Divine Comedy', '9780140448955', 1320, 'AVAILABLE', 'https://m.media-amazon.com/images/I/51i-9SGWr-L._AC_UF1000,1000_QL80_.jpg'),
+       ('Les Misérables', '9780451419439', 1862, 'AVAILABLE', 'https://m.media-amazon.com/images/I/91p594CxSpL.jpg'),
+       ('Don Quixote', '9780060934347', 1605, 'AVAILABLE', 'https://m.media-amazon.com/images/I/61yifvabpVL._AC_UF1000,1000_QL80_.jpg'),
+       ('A Tale of Two Cities', '9780486406516', 1859, 'BORROWED', 'https://m.media-amazon.com/images/I/91ptoYiDzeL.jpg'),
        ('The Brothers Karamazov', '9780374528379', 1880, 'AVAILABLE',
-        'https://example.com/covers/brothers_karamazov.jpg'),
+        'https://m.media-amazon.com/images/I/81TnYwmy65L._AC_UF1000,1000_QL80_.jpg'),
        ('The Count of Monte Cristo', '9780140449267', 1844, 'BORROWED',
-        'https://example.com/covers/count_of_monte_cristo.jpg');
+        'https://m.media-amazon.com/images/I/71zcCb5PvuL._UF1000,1000_QL80_.jpg');
 
 INSERT INTO book_authors (book_id, author_id)
 VALUES (1, 1),   -- '1984' by George Orwell
@@ -87,17 +87,17 @@ VALUES (1, 1),   -- '1984' by George Orwell
        (17, 17), -- Wuthering Heights - Emily Brontë
        (18, 18), -- The Picture of Dorian Gray - Oscar Wilde
        (19, 9),  -- The Hobbit - J.R.R. Tolkien
-       (20, 19), -- Animal Farm - George Orwell
-       (21, 20), -- Little Women - Louisa May Alcott
-       (22, 21), -- Dracula - Bram Stoker
-       (23, 22), -- Frankenstein - Mary Shelley
-       (24, 23), -- The Alchemist - Paulo Coelho
-       (25, 24), -- The Divine Comedy - Dante Alighieri
-       (26, 25), -- Les Misérables - Victor Hugo
-       (27, 26), -- Don Quixote - Miguel de Cervantes
-       (28, 27), -- A Tale of Two Cities - Charles Dickens
+       (20, 1), -- Animal Farm - George Orwell
+       (21, 19), -- Little Women - Louisa May Alcott
+       (22, 20), -- Dracula - Bram Stoker
+       (23, 21), -- Frankenstein - Mary Shelley
+       (24, 22), -- The Alchemist - Paulo Coelho
+       (25, 23), -- The Divine Comedy - Dante Alighieri
+       (26, 24), -- Les Misérables - Victor Hugo
+       (27, 25), -- Don Quixote - Miguel de Cervantes
+       (28, 26), -- A Tale of Two Cities - Charles Dickens
        (29, 12), -- The Brothers Karamazov - Fyodor Dostoevsky
-       (30, 28); -- The Count of Monte Cristo - Alexandre Dumas
+       (30, 27); -- The Count of Monte Cristo - Alexandre Dumas
 
 INSERT INTO borrowers (name, email, phone, address, status)
 VALUES ('John Doe', 'johndoe@example.com', '555-1234', '123 Elm Street, Springfield', 'ACTIVE'),
