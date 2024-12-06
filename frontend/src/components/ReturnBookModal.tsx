ee500e97865875748fb79fbab5a84ed131270775
Khang Huy <khatihuy125@gmail.com>
2024-12-06 21:19:48 +0700

    render record ui

---
+++ frontend/src/components/ReturnBookModal.tsx
@@ -0,0 +1,48 @@
+import { useRef } from "react";
+import { IoMdClose } from "react-icons/io";
+
+export default function ReturnBookModal({ handleCloseModal, handleSubmit }) {
+
+    const dateRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <div className="border-0 rounded-lg shadow-lg flex flex-col w-96 bg-white p-6">
+        {/*header*/}
+        <div className="flex items-center justify-between pb-4 pt-2 border-b-2 border-blueGray-200 rounded-t w-full">
+            <h3 className="text-lg font-semibold">Updating book</h3>
+            <button
+            className="border-0"
+            onClick={() => handleCloseModal()}
+            >
+            <span className="text-lg">
+                <IoMdClose />
+            </span>
+            </button>
+        </div>
+        {/*end header*/}
+        <div className="w-full mt-4">
+            <div className="flex flex-col text-sm">
+                <label className="ml-3 opacity-50">Return date</label>
+                <input className="mt-1 h-10 w-full rounded-lg pl-3 border-2 border-black font-semibold"
+                type="date" ref={dateRef}/>
+            </div>
+        </div>
+        <div className="flex justify-end gap-2 mt-4">
+            <button
+            type="button"
+            className=" bg-red-500 text-sm font-semibold text-white p-2 rounded-lg w-20"
+            onClick={() => handleCloseModal()}
+            >
+            Cancel
+            </button>
+            <button
+            type="button"
+            className=" bg-green-500 text-sm font-semibold text-white p-2 rounded-lg w-20"
+            onClick={() => handleSubmit({title: titleRef.current!.value, isbn: isbnRef.current!.value, publicationYear: yearRef.current!.value, authors: authorList.filter((author)=> author.authorId == Number(authorRef.current!.value))[0], coverImageUrl: imageRef.current!.value})}
+            >
+            Update
+            </button>
+        </div>
+    </div>
+  )
+}
--- frontend/src/pages/Record.tsx
+++ frontend/src/pages/Record.tsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react";
 import { Book } from "./Books";
 import axios from "axios";
+import Modal from "../components/Modal";
+import ReturnBookModal from "../components/ReturnBookModal";
 
 export interface Record {
   borrowingId: number,
   bookId: number,
   borrowerId: number,
-  borrowDate: string,
+  borrowedDate: string,
   dueDate: string,
-  returnDate: string,
+  returnedDate: string,
   status: string
 }
 
@@ -16,6 +18,8 @@ export default function Record() {
 
   const [bookList, setBookList] = useState<Book[]>([]);
   const [recordList, setRecordList] = useState<Record[]>([]);
+  const [isShowModalReturn, setIsShowModalReturn] = useState(false);
+  const [currentRecordId, setCurrentRecordId] = useState<number>();
 
   const handleGetBooks = async () => {
     try {
@@ -41,14 +45,87 @@ export default function Record() {
     }
   };
 
+  const handleOpenModalReturn = (id: number) => {
+    setCurrentRecordId(id);
+    setIsShowModalReturn(true);
+  }
+
+  const handleCloseModalReturn = () => {
+    setIsShowModalReturn(false);
+  };
+
+
   useEffect(() => {
     handleGetBooks();
     handleGetRecords();
   }, []);
 
   return (
-    <div>
-      Record page
+    <div className="flex flex-col w-full h-screen p-4">
+      <div className="mb-4">
+        <p className="text-center text-3xl font-bold">Record List</p>
+      </div>
+      <div className="relative overflow-x-auto w-full">
+          <table className="w-full text-left rtl:text-right text-gray-500">
+              <thead className="text-base text-gray-700 uppercase bg-gray-100">
+                  <tr>
+                      <th className="px-6 py-3">
+                          Book Title
+                      </th>
+                      <th className="px-6 py-3">
+                          Borrower's ID
+                      </th>
+                      <th className="px-6 py-3">
+                          Borrow Date
+                      </th>
+                      <th className="px-6 py-3">
+                          Due Date
+                      </th>
+                      <th className="px-6 py-3">
+                          Return Date
+                      </th>
+                      <th className="px-6 py-3">
+                          Status
+                      </th>
+                      <th className="px-6 py-3">
+                          Action
+                      </th>
+                  </tr>
+              </thead>
+              <tbody>
+                {recordList.map((record) => (
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 text-base">
+                      <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                          {bookList.find(book => book.bookId === record.bookId)?.title}
+                      </th>
+                      <td className="px-6 py-4">
+                          {record.borrowerId}
+                      </td>
+                      <td className="px-6 py-4">
+                          {record.borrowedDate}
+                      </td>
+                      <td className="px-6 py-4">
+                          {record.dueDate}
+                      </td>
+                      <td className="px-6 py-4">
+                          {record.returnedDate || "None"}
+                      </td>
+                      <td className="px-6 py-4">
+                          {record.status}
+                      </td>
+                      <td className="px-6 py-4">
+                          <button className={`w-20 border-2 border-blue-500 p-2 text-base font-semibold rounded-lg bg-blue-500 text-white ${record.status != "RETURNED" && "hover:bg-white hover:text-black"}  ${record.status == "RETURNED" && "cursor-not-allowed opacity-50"}`} disabled = {record.status == "RETURNED"? true:false}onClick={() => handleOpenModalReturn(record.borrowingId)}>Return</button>
+                      </td>
+                  </tr>
+                ))}
+              </tbody>
+          </table>
+      </div>
+
+      <Modal isShowModal={isShowModalReturn}>
+        <ReturnBookModal handleCloseModal={handleCloseModalReturn} handleSubmit={handleGetRecords}/>
+      </Modal>
+
     </div>
   )
 }
