8c6b90c172ea5d919f27dd5f421dbd0ab8da77a8
Kong Dinh <alexvinh2911@gmail.com>
2024-12-06 15:21:19 +0700

    Update BorrowerRepository to delete no use methods.

--- backend/src/main/java/com/example/backend/repository/BorrowerRepository.java
+++ backend/src/main/java/com/example/backend/repository/BorrowerRepository.java
@@ -12,13 +12,6 @@ import java.util.Optional;
 
 @Repository
 public interface BorrowerRepository extends JpaRepository<Borrower, Long> {
-
-    @Query("SELECT b FROM Borrower b WHERE b.name = :name")
-    Optional<Borrower> findByName(@Param("name") String name);
-
-    @Query("SELECT b FROM Borrower b WHERE b.borrowerId = :borrowerId")
-    List<Borrower> findBorrowerById(@Param("borrowerId") Long borrowerId);
-
     @Query("SELECT br FROM Borrowing br WHERE br.borrower.borrowerId = :borrowerId")
     List<Borrowing> findBorrowingByBorrowerId(@Param("borrowerId") Long borrowerId);
 }
